46e2f1b2fe6131a35a59f5194a8a3ddbd6ccde79
vineethpawar <vineethpawar99@gmail.com>
2023-12-11 03:36:36 +0530

    chore: added tag line

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,11 +4,11 @@ const Home = () => {
     return (
         <section className='insetShadow flex flex-1 w-full  items-center justify-items-center '>
 
-            <div className="text-center w-full gap-10 flex flex-col">
+            <div className="text-center w-full gap-10 flex flex-col items-center p-4">
 
-                <div className='flex flex-col gap-4'>
-                    <h1 className="sm:text-2xl md:text-6xl  font-extrabold text-center textGlow">Dive into the coolest styles</h1>
-                    <h3 className="sm:text-xl md:text-xl  font-bold text-center capitalize text-gray-400">create &middot; customise  &middot; copy</h3>
+                <div className='flex flex-col gap-4 max-w-screen-lg '>
+                    <h1 className="sm:text-2xl md:text-6xl  font-extrabold text-center textGlow">Elevate Your Design Game with Trendy Hover Styles</h1>
+                    <h3 className="sm:text-xl md:text-xl  font-bold text-center capitalize text-slate-500">create &middot; customise  &middot; <span className='cursor-copy text-gray-100 bg-blue-600 px-1'>copy</span></h3>
                 </div>
 
                 <div>
