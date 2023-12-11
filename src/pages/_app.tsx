4d04433c3f655118416c47668806bc1ad235bce9
vineethpawar <vineethpawar99@gmail.com>
2023-12-12 01:03:56 +0530

    fix: fontawesome css delayed load fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import "@/styles/globals.css";
+
 import type { AppProps } from "next/app";
 import { Montserrat } from "next/font/google";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
 
 const montserrat = Montserrat({
   weight: "400",
---
+++ src/utils/config.ts
@@ -0,0 +1,3 @@
+export const ENDPOINTS = {
+  githubRepo: "https://api.github.com/repos/vineethpawar/hover-css-generator",
+};
