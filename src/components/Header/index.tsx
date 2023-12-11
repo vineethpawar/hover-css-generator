ee9962bb03848c93dde78fd4fea7169bdd1819e1
vineethpawar <vineethpawar99@gmail.com>
2023-12-11 06:02:38 +0530

    fix: minor css fix header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,15 +2,22 @@ import React from "react";
 import Icon from "../Icon";
 import Link from "next/link";
 import Image from "next/image";
-import logo from "@/assets/images/logo.png";
+import logo from "@/assets/images/logoWhite.png";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
-    <div className="flex w-full p-3 justify-between gap-4 items-center">
-      <Image src={logo} width={150} className="relative" alt="logo" />
-      <div className="flex items-center gap-3">
+    <div className="flex w-full align-end justify-between gap-4 p-3">
+      <Link href={"/"}>
+        <Image
+          src={logo}
+          width={150}
+          className="cursor-pointer select-none"
+          alt="logo"
+        />
+      </Link>
+      <div className="flex items-center gap-3 -mt-5">
         <Link
           target="_blank"
           href={"https://github.com/vineethpawar/hover-css-generator"}
